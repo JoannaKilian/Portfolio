e63ce51f7733fc89d185d8f9a9710b01901958e3
Joanna Borkala-Kilian <joanna.kilian@foundbycode.dev>
2022-05-25 19:43:02 +0200

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { NavComponent } from "./nav/nav.component";
+import { HeaderComponent } from "./header/header.component";
+import { AboutComponent } from "./about/about.component";
+import { EducationComponent } from "./education/education.component";
+import { ExperienceComponent } from "./experience/experience.component";
+import { SkillsComponent } from "./skills/skills.component";
+import { ProjectsComponent } from "./projects/projects.component";
+import { ContactComponent } from "./contact/contact.component";
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HeaderComponent,
+    AboutComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    ContactComponent,
+    StartComponent,
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
