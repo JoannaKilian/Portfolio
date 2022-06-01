ac472fc581c951538947c89f1db2941cab15dc27
Joanna Borkala-Kilian <joanna.kilian@foundbycode.dev>
2022-06-01 16:17:31 +0200

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,21 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { NavComponent } from "./nav/nav.component";
 import { HeaderComponent } from "./header/header.component";
 import { AboutComponent } from "./about/about.component";
-import { EducationComponent } from "./education/education.component";
-import { ExperienceComponent } from "./experience/experience.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { ContactComponent } from "./contact/contact.component";
-import { StartComponent } from './start/start.component';
-import { SharpComponent } from './sharp/sharp.component';
+import { SharpComponent } from "./sharp/sharp.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
     HeaderComponent,
     AboutComponent,
-    EducationComponent,
-    ExperienceComponent,
     SkillsComponent,
     ProjectsComponent,
     ContactComponent,
-    StartComponent,
     SharpComponent,
   ],
   imports: [BrowserModule],
