4d436d03f0a1d0cfb2f73cc9e7a5412ae6227d51
Joanna Borkała-Kilian <joanna.borkala-kilian@oroox.com>
2023-05-09 10:50:37 +0200

    update information

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,48 +1,48 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: "app-projects",
-  templateUrl: "./projects.component.html",
-  styleUrls: ["./projects.component.css"],
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrls: ['./projects.component.css'],
 })
 export class ProjectsComponent implements OnInit {
   projects = [
     {
-      title: "My portfolio",
-      description: "CREATED FOR YOU to show who I am and what I can do.",
-      link: "Visit site",
-      href: "#",
+      title: 'Animation',
+      description: 'While exploring the amazing possibilities of CSS.',
+      link: 'Visit site',
+      href: 'https://joannakilian.github.io/animation-cubes-3D/',
     },
     {
-      title: "English Game",
+      title: 'Dream',
+      description: 'Difference scroll effect using JavaScript.',
+      link: 'Visit site',
+      href: 'https://joannakilian.github.io/www-Dream/',
+    },
+    {
+      title: 'English Game',
       description:
-        "The game is created in Angular/TypeScript, connected with outside API data.",
-      link: "Visit site",
-      href: "https://joannakilian.github.io/LearningGame/",
+        'The game is created in Angular/TypeScript, connected with outside API data.',
+      link: 'Visit site',
+      href: 'https://joannakilian.github.io/LearningGame/',
     },
     {
-      title: "Dream",
-      description: "Difference scroll effect using pure JavaScript.",
-      link: "Visit site",
-      href: "https://joannakilian.github.io/www-Dream/",
+      title: 'Hangman',
+      description: 'Game in Java Script',
+      link: 'Visit site',
+      href: 'https://joannakilian.github.io/game-Hangman/',
     },
     {
-      title: "Animation",
-      description: "While exploring the amazing possibilities of CSS.",
-      link: "Visit site",
-      href: "https://joannakilian.github.io/animation-cubes-3D/",
+      title: 'My portfolio',
+      description: 'CREATED FOR YOU to show who I am and what I can do.',
+      link: 'Visit site',
+      href: '#',
     },
     {
-      title: "Blog",
-      description: "Written in English what I learn. Just for a fan.",
-      link: "Visit site",
-      href: "https://foundbycode.dev/",
-    },
-    {
-      title: "Other",
-      description: "All projects are on GitHub",
-      link: "Visit site",
-      href: "https://github.com/JoannaKilian",
+      title: 'Present work on dashboard',
+      description: 'Using knowledge from working with Angular',
+      link: 'Code',
+      href: 'https://github.com/JoannaKilian/dashboard',
     },
   ];
 
